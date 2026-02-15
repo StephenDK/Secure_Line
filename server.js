ee6978b4f146d59566bcca223a11e97afd00ef0b
StephenDK <stephendklein08@gmail.com>
2026-02-15 11:57:27 -0800

    Fly.io Deployment Preperation

--- server.js
+++ server.js
@@ -117,7 +117,9 @@ wss.on("connection", (ws) => {
   });
 });
 
-server.listen(3000, () => {
-  console.log("🚀 Secure line running on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`🚀 Secure line running on http://localhost:${PORT}`);
   printSlots();
 });
